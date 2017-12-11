1c7d55ef4dc9e69b78375e3debeaebd4bdb6949d
Samuel Mosley <samueljmosley@gmail.com>
2017-12-11 14:42:03 -0600

    first commit with hex generator

---
+++ hex-generator/app.js
@@ -0,0 +1,21 @@
+//generate color
+function getColor(){
+    let color = '#' + Math.random().toString(16).slice(2,8);
+    //console.log(color);
+    return color;
+}
+//set the background
+function setBackground(){
+    let bgColor = getColor();
+    document.body.style.background = bgColor;
+    document.getElementById('colorDisplay').innerHTML = bgColor;
+}
+
+//get key press
+document.body.onkeyup = (e) => {
+    if(e.keyCode = 32){
+        setBackground();
+    }
+}
+
+setBackground();
